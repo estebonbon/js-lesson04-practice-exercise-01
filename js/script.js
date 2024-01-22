1b3c3536e180d438729bc48dd50c7aa442e848b4
estebonbon <estesarmiento1@gmail.com>
2024-01-22 10:42:41 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var footerBackground = document.querySelector(".contact");
+footerBackground.style.backgroundColor = "#d7ddf3";
+
+var capitals = document.querySelector(".tagline");
+capitals.style.textTransform = "capitalize";
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+
+var everyDay = document.querySelector(".contact h3");
+everyDay.innerHTML = "We're here for you <em>every day</em> if the weak.";
